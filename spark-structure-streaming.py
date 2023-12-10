f6b58d5c32d636a714cac832e27e9e587934af99
Marwan Ali <96815791+marwanali213@users.noreply.github.com>
2023-12-10 16:24:08 +0200

    Add files via upload

--- spark-structure-streaming.py
+++ spark-structure-streaming.py
@@ -33,6 +33,32 @@ def insert_into_phpmyadmin(row):
     conn.commit()
     conn.close()
 
+def insertphpmyadmin(row):
+    host = "localhost"
+    port = 3306
+    database = "big_data"
+    username = "root"
+    password = ""
+
+    conn = pymysql.connect(host=host, port=port, user=username, passwd=password, db=database)
+    cursor = conn.cursor()
+
+    # Extract the required columns from the row
+
+    column1_value = row.date
+    column3_value = row.continent
+    column2_value = row.avg_cases_per_continent
+
+    # Prepare the SQL query to insert data into the table
+    sql_query = f"INSERT INTO virus(`date`,`continent`, `avg_cases_per_continent`) VALUES ('{column1_value}', '{column3_value}', '{column2_value}')"
+
+    # Execute the SQL query
+    cursor.execute(sql_query)
+
+    # Commit the changes
+    conn.commit()
+    conn.close()
+
 # Create a Spark session
 spark = SparkSession.builder \
     .appName("KafkaConsumer") \
@@ -128,6 +154,26 @@ query = df3.writeStream \
     .outputMode("complete") \
     .format("console") \
     .foreach(insert_into_phpmyadmin) \
+    .start()
+
+# TASK TWO : TRACKING VIRUS SPREAD :->>>> MENNA , MARIAM ALY
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "dull") \
+    .load() \
+    .select(from_json(col("value").cast("string"), schema).alias("data")) \
+
+# Select specific columns from "data"
+df2 = df.select("data.date","data.continent", "data.new_cases")
+df = df2.groupBy("date","continent").agg(avg("new_cases").alias("avg_cases_per_continent"))
+df3 = df.select("continent","date","avg_cases_per_continent")
+
+# Convert the value column to string and display the result
+query = df3.writeStream \
+    .outputMode("complete") \
+    .format("console") \
+    .foreach(insertphpmyadmin) \
     .start()
 
 # Wait for the query to finish
